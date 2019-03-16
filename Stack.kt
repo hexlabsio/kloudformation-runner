15a32a4af03ec23924b883f640d34b17fabbfaa8
Chris Barbour <chris.barbour03@gmail.com>
2019-03-16 17:03:04 +0000

    fixing args

--- Stack.kt
+++ Stack.kt
@@ -2,7 +2,7 @@ import io.kloudformation.KloudFormation
 import io.kloudformation.StackBuilder
 import io.kloudformation.resource.s3.bucket
 
-class Kloudformation: StackBuilder {
+class Stack: StackBuilder {
     override fun KloudFormation.create() {
         bucket {
             bucketName("kloudformation-runner")
