f64b2aca3a10f0e013489e85088a8db1167e1f1a
Chris Barbour <chris.barbour03@gmail.com>
2019-03-17 20:52:07 +0000

    updated installation script

--- install-kloudformation.sh
+++ install-kloudformation.sh
@@ -1,3 +1,10 @@
 #!/bin/bash +e
 
-echo hello world
+mkdir -p /usr/local/bin/kloudformation-install
+curl -s https://install.kloudformation.hexlabs.io/kloudformation.sh -o /usr/local/bin/kloudformation-install/kloudformation.sh
+ln -F -s /usr/local/bin/kloudformation-install/kloudformation.sh /usr/local/bin/kloudformation
+chmod +x /usr/local/bin/kloudformation
+
+echo "KloudFormation installed to /usr/local/bin/kloudformation"
+
+kloudformation help
